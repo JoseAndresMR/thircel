866d511d4f106807c35ce2e6128bd97ce535abdf
JoseAndresMR <36242471+JoseAndresMR@users.noreply.github.com>
2025-08-08 20:51:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, updateDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 import {
   getFirestore,
   doc,
